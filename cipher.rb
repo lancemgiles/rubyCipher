19b925ad40f66a917134625d93b3bf4818e22c4c
Lance <dogmandevices@gmail.com>
2024-04-03 22:39:20 -0500

    only change letters to ascii

--- cipher.rb
+++ cipher.rb
@@ -1,13 +1,31 @@
+require 'pry-byebug'
+
 def caesar_cipher(string, num)
-	asciiShift = string.chars.map { |c| c.ord }.map { |n| (n + num).chr}
+	getChars = string.chars
+	inclusions = getChars.reject{ |c| (c.ord < 65) || (c.ord > 122)}
+	getAscii = inclusions.map { |c| c.ord}
+	ascii = []
+	modify = getChars.each{
+		|c|
+		if (c.ord < 65) || (c.ord > 122)
+			ascii.push(c)
+		else
+			ascii.push(c.ord)
+		end
+	}
+	p ascii
+	# shift = modify.map { |n| (n + num).chr}
+	# shift.join
 	# loop through asciiShift to find if any ascii values have moved above or below
-	# letter ranges and adjust them accordingly
+	# letter ranges
+	# items outside of that range should remain in the array,
+	# but not be modified
 	# join characters back into a string
 end
 
 
-input = "zab"
+input = "{zab!"
 puts caesar_cipher(input, 1)
 
-input2 = "ZAB"
-puts caesar_cipher(input2, 1)
+# input2 = "{ZAB!"
+# puts caesar_cipher(input2, 1)
