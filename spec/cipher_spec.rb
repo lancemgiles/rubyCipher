14599bd434259859b959553953b49656a82525c0
lance <dogmandevices@gmail.com>
2024-04-23 17:59:29 -0500

    create rspec tests

---
+++ spec/cipher_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require '../cipher'
+
+describe CaesarCipher do
+  describe '#cipher' do
+    it 'returns a message shifted by a given factor' do
+      cipher = CaesarCipher.new
+      expect(cipher.cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+    end
+  end
+end
